d945542e44add1415112ef8463f4c46bb36918a5
Ana Bennett <anabennett1111@gmail.com>
2024-01-04 16:18:07 -0600

    Get the font to import correctly in the header.scss

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,10 +4,12 @@ import '../Header/Header.scss';
 
 const Header = () => {
     return (
-        <>
-        <h2>This is the header</h2>
-        </>
-    )
+      <>
+        <h2 className="header-title">
+          We make your music sound extraordinary.
+        </h2>
+      </>
+    );
 };
 
 export default Header;
