9ca225c1ded09b30e5da38e0030198c97648a1a4
Ana Bennett <anabennett1111@gmail.com>
2024-01-04 15:29:48 -0600

    Added font into the typogoraphy file

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../Header/Header.scss';
+
+
+const Header = () => {
+    return (
+        <>
+        <h2>This is the header</h2>
+        </>
+    )
+};
+
+export default Header;
