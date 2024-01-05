3e71ea5a99eff1e9b09d4f627d17d01c228e24fc
Ana Bennett <anabennett1111@gmail.com>
2024-01-05 14:39:12 -0600

    Completed design for the middle section on the mobile view

--- src/Components/Middle/Middle.jsx
+++ src/Components/Middle/Middle.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../Middle/Middle.scss'
 import bgpattern2 from '../../assets/bgpattern2.svg'
-
+import illustration from '../../assets/illustration.png';
 const Middle = () => {
     return (
       <>
-        <section className="middle-section">
+        <section className="middle-section"> 
           <img className="middle-section-image"src={bgpattern2} />
+          <img className="middle-section-phone" src={illustration} />
         </section>
       </>
     );
