9556a53448c68d88dbe9762fcf53b063f061dd4b
Ana Bennett <anabennett1111@gmail.com>
2024-01-05 11:35:03 -0600

    Add styling to the header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,25 @@
 import React from "react";
-import '../Header/Header.scss';
-
+import "../Header/Header.scss";
+import logo from "../../assets/logo.svg";
+import "../../styles/_global.scss";
 
 const Header = () => {
-    return (
-      <>
-        <h2 className="header-title">
+  return (
+    <>
+      <div className="header">
+        <img src={logo} alt="Logo" className="header-logo" />
+        <h1 className="header-statement">
           We make your music sound extraordinary.
-        </h2>
-      </>
-    );
+        </h1>
+        <p className="header-paragraph">
+          A system audio equalizer specifically designed for Android and iOS.
+          Freely tune the way your music sounds with a professional grade
+          parametric EQ & volume mixer. Control bass, mids, treble, gain
+          control, reverb, and more!
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default Header;
