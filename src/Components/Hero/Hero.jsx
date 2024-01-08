aa832d47dd634d86b147b2d70bb9d741a2de2b5d
Ana Bennett <anabennett1111@gmail.com>
2024-01-08 11:23:50 -0600

    Complete styling of the buttons on the hero component

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,37 +1,33 @@
-import React from 'react';
-import '../Hero/Hero.scss';
-import apple from '../../assets/apple.svg';
-import android from '../../assets/android.svg';
+import React from "react";
+import "../Hero/Hero.scss";
+import apple from "../../assets/apple.svg";
+import android from "../../assets/android.svg";
 const Hero = () => {
-    return (
-      <>
-        <section className="hero-section">
-          <h2 className="hero-title">Premium EQ</h2>
-          <p className="hero-paragraph">
-            Get expert-level control with a robust equalizer, volume mixer, and
-            spatial audio. Take your listening experience to a whole new level
-            and access all our incredible features!
-          </p>
-          <p className="hero-price">
-            <span className="hero-price-bold">$4</span>/month
-          </p>
-          <div className="hero-buttons">
-            <button className="download-button">
-              <img src={apple} alt="iOS" className="button-icon" />
-              iOS Download
-            </button>
-            <button className="download-button">
-              <img
-                src={android}
-                alt="Android"
-                className="button-icon"
-              />
-              Android Download
-            </button>
-          </div>
-        </section>
-      </>
-    );
+  return (
+    <>
+      <section className="hero-section">
+        <h2 className="hero-title">Premium EQ</h2>
+        <p className="hero-paragraph">
+          Get expert-level control with a robust equalizer, volume mixer, and
+          spatial audio. Take your listening experience to a whole new level and
+          access all our incredible features!
+        </p>
+        <p className="hero-price">
+          <span className="hero-price-bold">$4 </span>/ month
+        </p>
+        <div className="hero-buttons">
+          <button className="download-button ios">
+            <img src={apple} alt="iOS" className="button-icon" />
+            iOS Download
+          </button>
+          <button className="download-button android">
+            <img src={android} alt="Android" className="button-icon" />
+            Android Download
+          </button>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Hero;
